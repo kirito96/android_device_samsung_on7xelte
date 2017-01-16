b6da728dca76fbaf39a0968bb5dec9e15b17a4e1
grace5921 <androidlover5842@gmail.com>
2017-01-16 14:49:34 +0530

    update mounts

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,8 @@
 add_lunch_combo omni_on7xelte-eng
 function mkazip_on7xelte
 {
-cp device/samsung/on7xelte/META-INF.zip out/target/product/on7xelte/META-INF.zip 
+zip META-INF.zip -r device/samsung/on7xelte/META-INF 
+mv META-INF.zip out/target/product/on7xelte/META-INF.zip
 cd out/target/product/on7xelte/
 unzip META-INF.zip 
 rm META-INF.zip 
